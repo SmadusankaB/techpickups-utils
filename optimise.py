f4e84f09ed9596ee4e65c183040c725790d86e78
SeNewGit <seemilyrose@gmail.com>
2024-06-18 12:59:35 +0530

    Just resize

--- optimise.py
+++ optimise.py
@@ -1,6 +1,34 @@
 import os
 from PIL import Image
 
+def resize_and_convert_images(source_folder, max_width=900, max_height=900):
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            if file.lower().endswith(('.jpg', '.jpeg', '.png', '.webp')):
+                file_path = os.path.join(root, file)
+                webp_file_path = os.path.splitext(file_path)[0] + '.webp'
+                try:
+                    with Image.open(file_path) as img:
+                        # Calculate the aspect ratio
+                        aspect_ratio = img.width / img.height
+
+                        # Determine new dimensions based on max width and max height
+                        if img.width > max_width or img.height > max_height:
+                            if aspect_ratio > 1:  # Landscape orientation
+                                new_width = min(max_width, img.width)
+                                new_height = int(new_width / aspect_ratio)
+                            else:  # Portrait orientation
+                                new_height = min(max_height, img.height)
+                                new_width = int(new_height * aspect_ratio)
+
+                            # Resize the image
+                            img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+                        # Save the image as WebP
+                        img.save(webp_file_path, 'webp', optimize=True, quality=85)
+                    print(f'Converted and resized {file_path} to {webp_file_path}')
+                except Exception as e:
+                    print(f'Failed to convert {file_path}: {e}')
 
 def fill_and_resize_images_with_background(source_folder, max_width=800, max_height=450, background_color=(240, 240, 240)):
     """
@@ -49,7 +77,6 @@ def fill_and_resize_images_with_background(source_folder, max_width=800, max_hei
 
 
 if __name__ == "__main__":
-    fill_and_resize_images_with_background("netzz/resize")
-    # crop_from_l_t_r_b(source_folder)
-    # crop_from_r_b("netzz/crop_from_r_b")
+    # fill_and_resize_images_with_background("netzz/resize")
+    resize_and_convert_images("netzz/just_resize")
 
